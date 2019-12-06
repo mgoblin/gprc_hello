8cdd1f86316e46f9d5a8c390d019f7766babd6e8
mgoblin <mike@localhost.localdomain>
2019-12-06 06:02:58 -0800

    build from dockerfile

--- build/build-from-dockerfile.sh
+++ build/build-from-dockerfile.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+echo "Build server.go"
+
+GOOS=linux CGO_ENABLED=0 go build -a -ldflags="-s -w" ../mg.ru/hello/server/server.go
+
+echo "Build server container using buildah from dockerfile"
+
 buildah bud 
 
